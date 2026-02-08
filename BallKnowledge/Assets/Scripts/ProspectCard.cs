fb4943a4535d8bbbe654e2cd6663f351febdc338
LDelDeo <ljdeldeo@email.NEIT.edu>
2026-02-07 20:16:48 -0500

    Employee Card Inheritance / Derived Classes

---
+++ BallKnowledge/Assets/Scripts/ProspectCard.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class ProspectCard : EmployeeCard
+{
+    #region Prospect Visuals
+    [Header("Prospect Card Visuals")]
+    [SerializeField] TMP_Text statOne;
+    [SerializeField] TMP_Text statTwo;
+    [SerializeField] TMP_Text statThree;
+    [SerializeField] TMP_Text statFour;
+    [SerializeField] TMP_Text statFive;
+    #endregion
+}
--- BallKnowledge/Assets/Scripts/TestGeneration.cs
+++ BallKnowledge/Assets/Scripts/TestGeneration.cs
@@ -10,6 +10,9 @@ public class TestGeneration : MonoBehaviour
     [Header("Prefabs")]
     [SerializeField] GameObject employeeCard;
     [SerializeField] Transform rosterGrid;
+    [SerializeField] GameObject prospectCard;
+    [SerializeField] Transform prospectLayout;
+    
 
     private void Start()
     {
@@ -17,15 +20,17 @@ public class TestGeneration : MonoBehaviour
         EmployeeLists employeeLists = new EmployeeLists();
 
         var employeeCardObject = employeeCard.GetComponent<EmployeeCard>();
+        var prospectCardObject = new ProspectCard();
+        prospectCardObject = employeeCard.GetComponent<ProspectCard>();
 
         // Create Employees for Roster (We can use this for when you start the game)
-        CreateAnEmployee(rosterCount, employeeFactory, employeeLists, employeeLists.currentRoster, employeeCardObject);
+        CreateAnEmployee(rosterCount, employeeFactory, employeeLists, employeeLists.currentRoster, employeeCardObject, rosterGrid);
 
         // Create Employees for Draft Class (Create this list for the annual draft in the offseason)
-        CreateAnEmployee(draftClassSize, employeeFactory, employeeLists, employeeLists.draftClass, employeeCardObject);
+        CreateAnEmployee(draftClassSize, employeeFactory, employeeLists, employeeLists.draftClass, prospectCardObject, prospectLayout);
     }
 
-    private void CreateAnEmployee(int employeeCount, EmployeeFactory employeeFactory, EmployeeLists employeeLists, List<Employee> listToAddTo, EmployeeCard employeeCard)
+    private void CreateAnEmployee(int employeeCount, EmployeeFactory employeeFactory, EmployeeLists employeeLists, List<Employee> listToAddTo, EmployeeCard employeeCard, Transform layout)
     {
         for (int i = 0; i < employeeCount; i++)
         {
@@ -35,7 +40,7 @@ public class TestGeneration : MonoBehaviour
         foreach (var employee in listToAddTo)
         {
             employeeCard.GetEmployeeStats(employee);
-            Instantiate(employeeCard, rosterGrid);
+            Instantiate(employeeCard, layout);
         }
     }
 }
